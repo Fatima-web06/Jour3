d2ea19d3ff44de34dbaf2e603945127f62fb5b9e
Fatima-web06 <fatima.fall@epitech.digital>
2024-10-17 11:28:33 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,15 +1,13 @@
 function findHousing(housingToFind, housingList) {
-    let index = 0;
-    let lowerCaseHousingToFind = housingToFind.toLowerCase();
-    
-    while (index < housingList.length) {
-        if (housingList[index].toLowerCase() === lowerCaseHousingToFind) {
-            return [index + 1, housingList[index]];
+    housingToFind = housingToFind.toLowerCase();
+    let i = 0;
+    while (i < housingList.length) {
+        if (housingList[i].toLowerCase() === housingToFind) {
+            return [i + 1, housingList[i]];
         }
-        index++;
+        i++;
     }
-    
     return null;
 }
-
-displayResult(findHousing("Igloo", ["Flat", "House", "Hut", "House", "Igloo"]))
+ 
+console.log("I'm debugging")
